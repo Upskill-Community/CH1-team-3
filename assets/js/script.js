f5386053f8ff5a545c9520f8c317385b5437519e
Chinonso Vincent <nchinonsovincent@gmail.com>
2022-07-05 23:50:30 +0100

    added some finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@ const links = document.querySelector(".nav-list");
 const navBtnDOM = document.querySelector(".nav-btn");
 
 navBtnDOM.addEventListener("click", () => {
-    links.classList.toggle("show-links");
+	links.classList.toggle("show-links");
 });
