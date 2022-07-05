ea2ca365fa824251662f142e797f4d4b5c8dc6ac
jumiej <akinyodejummy177@gmail.com>
2022-07-05 23:35:32 +0100

    js update

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const links = document.querySelector(".nav-list");
+const navBtnDOM = document.querySelector(".nav-btn");
+
+navBtnDOM.addEventListener("click", () => {
+    links.classList.toggle("show-links");
+});
